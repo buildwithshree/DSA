370270f1f7012602858c4d9e81c8b06447f944f2
Shreejal Mehta <igromeox26@gmail.com>
2026-06-20 16:01:47 +0530

    Add Fibonacci Number solution - Recursion basics

---
+++ string/FibonacciNumber.java
@@ -0,0 +1,13 @@
+package string;
+
+class Solution {
+    public int fib(int n) {
+
+        // base cases
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        // recursive case
+        return fib(n - 1) + fib(n - 2);
+    }
+}
